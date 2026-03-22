fa903aa318f38254ea92ea6d0461cf447adf1d87
leunsoo <98530935+leunsoo@users.noreply.github.com>
2026-03-23 02:11:02 +0900

    chore: 초기 기본 프로젝트 정리, tailwind, tanstackRouter 라이브러리 추가

--- uandup_voca/vite.config.ts
+++ uandup_voca/vite.config.ts
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    TanStackRouterVite({
+      routesDirectory: "./src/routes",
+      generatedRouteTree: "./src/app/routeTree.gen.ts",
+    }),
+    tailwindcss(),
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
   ],
-})
+});
