6008d3bc3c1dc5f29bddf2a166b39696bb4f87e4
leunsoo <98530935+leunsoo@users.noreply.github.com>
2026-06-03 22:35:06 +0900

    chore: 페이지 이동 시 scroll 초기화

--- uandup_voca/src/routes/__root.tsx
+++ uandup_voca/src/routes/__root.tsx
@@ -1,10 +1,21 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { createRootRoute, Outlet, useLocation } from '@tanstack/react-router';
+import { useLayoutEffect } from 'react';
 import { QueryProvider } from '@/app/providers/QueryProvider';
 
 export const Route = createRootRoute({
-  component: () => (
-    <QueryProvider>
-      <Outlet />
-    </QueryProvider>
-  ),
+  component: function RootLayout() {
+    const { pathname } = useLocation();
+
+    // SPA 특성상 라우트 이동 시 window 스크롤이 자동으로 초기화되지 않으므로
+    // pathname 변경 시 최상단으로 리셋한다.
+    useLayoutEffect(() => {
+      window.scrollTo({ top: 0, left: 0, behavior: 'instant' });
+    }, [pathname]);
+
+    return (
+      <QueryProvider>
+        <Outlet />
+      </QueryProvider>
+    );
+  },
 });
